38dc86c7568ba2868399a48bab24a67630e6500f
daniellejusayan <6731503051@lamduan.mfu.ac.th>
2025-09-01 10:15:17 +0000

    done

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
         return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
       }
       users.put(user.getUsername(), user);
-      return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+      return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 
     //GET to list all the users
